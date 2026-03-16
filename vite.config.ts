4cd47f254358d9bf57f4a3ad7750d873368c1023
Amanjot Singh <149322821+amanjot73@users.noreply.github.com>
2026-03-16 23:00:58 +0530

    hello

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
-  base:"/",
+  base: "/amanjot73.github.io/",
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
